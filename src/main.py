1f7726da8af2e9a473f030c7d676069ad4ec5986
JayPycentric <jadeon@pycentric.co.za>
2026-04-22 20:33:01 +0200

    feat(nav): Add page navigation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 import streamlit as st
 from pages.home import display_home
+from pages.about import display_about
 
-display_home()
+# display_home()
+# display_about()
+
+
+pg = st.navigation([
+    st.Page(display_home, title="home", icon=":material/home:"),
+    st.Page(display_about, title="About", icon=":material/person:")
+])
+
+pg.run()
