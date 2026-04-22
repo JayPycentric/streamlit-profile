672ed235de5f8bc8537c1fbed05b08d8ccf1dfd8
JayPycentric <jadeon@pycentric.co.za>
2026-04-22 22:59:49 +0200

    feat(home): add page link button for contact and projects

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,16 +1,24 @@
 # This is the portfolio home page
 import streamlit as st
 
+from pages.projects import display_projects
+from pages.contact import display_contact
 
 def display_home():
-    col1, col2 = st.columns([1, 1], vertical_alignment="center")
+    text, image = st.columns([1, 1], vertical_alignment="center")
 
-    with col1:
+    with text:
         st.title("Nice to meet You! I'm Jadeon Miller")
         st.subheader("Graduate Software Developer | Johannesburg")
         st.write("I build scalable web applications and explore cloud infrastructure.")
-        if st.button("Contact Me"):
-            st.info("Add contact field link LATER.")
 
-    with col2:
+        btn1, btn2, btn_ = st.columns([3, 3, 3])
+        with btn1:
+            if st.button("Contact Me", type="primary"):
+                st.switch_page(st.Page(display_contact))
+        with btn2:
+            if st.button("Projects", type="primary"):
+                 st.switch_page(st.Page(display_projects))
+
+    with image:
         st.image("resources/images/Gemini_Avatar_Image.png")
