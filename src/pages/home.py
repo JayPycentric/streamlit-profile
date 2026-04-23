5874868c268e14042c655bf69b5002eaa6d08154
JayPycentric <jadeon@pycentric.co.za>
2026-04-23 10:26:42 +0200

    style(home): add css to force max width

--- src/pages/home.py
+++ src/pages/home.py
@@ -5,6 +5,21 @@ from pages.projects import display_projects
 from pages.contact import display_contact
 
 def display_home():
+    st.markdown(
+        """
+        <style>
+            /* This targets the main content area and forces it to fill the screen */
+            .block-container {
+                max-width: 100% !important;
+                padding-left: 5rem !important;
+                padding-right: 5rem !important;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+
     text, image = st.columns([1, 1], vertical_alignment="center")
 
     with text:
@@ -12,13 +27,13 @@ def display_home():
         st.subheader("Graduate Software Developer | Johannesburg")
         st.write("I build scalable web applications and explore cloud infrastructure.")
 
-        btn1, btn2, btn_ = st.columns([3, 3, 3])
+        btn1, btn2, _ = st.columns([0.3, 0.3, 1.2], gap="xxsmall")
         with btn1:
-            if st.button("Contact Me", type="primary"):
+            if st.button("Contact Me", type="primary", use_container_width=True):
                 st.switch_page(st.Page(display_contact))
         with btn2:
-            if st.button("Projects", type="primary"):
+            if st.button("Projects", type="primary", use_container_width=True):
                  st.switch_page(st.Page(display_projects))
 
     with image:
-        st.image("resources/images/Gemini_Avatar_Image.png")
+        st.image("resources/images/Gemini_Generated_Image_ppinwyppinwyppin.png")
