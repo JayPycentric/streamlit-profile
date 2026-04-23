84d61c8f80d590223d5b84cbc47288dcc13bd19d
JayPycentric <jadeon@pycentric.co.za>
2026-04-23 10:26:19 +0200

    style(main): add page config styling

--- src/main.py
+++ src/main.py
@@ -4,6 +4,13 @@ from pages.about import display_about
 from pages.projects import display_projects
 from pages.contact import display_contact
 
+st.set_page_config(
+    page_title="Jadeon Miller | Portfolio",
+    page_icon=":material/person:",
+    layout="centered",             
+    initial_sidebar_state="expanded" 
+)
+
 pg = st.navigation([
     st.Page(display_home, title="home", icon=":material/home:"),
     st.Page(display_about, title="About", icon=":material/person:"),
